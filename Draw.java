2cc195a313415ced2271e108c6d97e3cc43f435b
hustinaaa <jraeviel@yahoo.com>
2019-02-14 15:13:55 +0800

    seperate

--- Draw.java
+++ Draw.java
@@ -26,7 +26,8 @@ public class Draw extends JComponent{
 	Monster monster4;
 	Monster monster5;
 	Monster monster6;
-
+ 	
+ 	Player player;
 
 	public Draw(){
 		monster1 = new Monster(200, 200);
@@ -44,95 +45,26 @@ public class Draw extends JComponent{
 			e.printStackTrace();
 		}
 	}
-
-	public void reloadImage(){
-		state++;
-
-		if(state == 0){
-			resource = getClass().getResource("run0.png");
-		}
-		else if(state == 1){
-			resource = getClass().getResource("run1.png");
-		}
-		else if(state == 2){
-			resource = getClass().getResource("run2.png");
-		}
-		else if(state == 3){
-			resource = getClass().getResource("run3.png");
-		}
-		else if(state == 4){
-			resource = getClass().getResource("run4.png");
-		}
-		else if(state == 5){
-			resource = getClass().getResource("run5.png");
-			state = 0;
-		}
-
-		try{
-			image = ImageIO.read(resource);
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-	}
-
-	public void attackAnimation(){
-		Thread thread1 = new Thread(new Runnable(){
-			public void run(){
-				for(int ctr = 0; ctr < 5; ctr++){
-					try {
-						if(ctr==4){
-							resource = getClass().getResource("run0.png");
-						}
-						else{
-							resource = getClass().getResource("attack"+ctr+".png");
-						}
-						
-						try{
-							image = ImageIO.read(resource);
-						}
-						catch(IOException e){
-							e.printStackTrace();
-						}
-				        repaint();
-				        Thread.sleep(100);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		});
-		thread1.start();
-	}
-
 	public void attack(){
-		attackAnimation();
+		player.attack();
 	}
 
 	public void moveUp(){
-		y = y - 5;
-		reloadImage();
-		repaint();
+		player.moveUp();
 	}
 
 	public void moveDown(){
-		y = y + 5;
-		reloadImage();
-		repaint();
+		player.moveDown();
 	}
 
 	public void moveLeft(){
-		x = x - 5;
-		reloadImage();
-		repaint();
+		player.moveLeft();
 	}
 
 	public void moveRight(){
-		x = x + 5;
-		reloadImage();
-		repaint();
+		player.moveRight();
 	}
-	
+
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setColor(Color.YELLOW);
@@ -144,6 +76,6 @@ public class Draw extends JComponent{
 		g.drawImage(monster3.image, monster1.xPos, monster3.yPos, this);
 		g.drawImage(monster4.image, monster2.xPos, monster4.yPos, this);
 		g.drawImage(monster5.image, monster1.xPos, monster5.yPos, this);
-		g.drawImage(monster6.image, monster2.xPos, monster6.yPos, this)3
+		g.drawImage(monster6.image, monster2.xPos, monster6.yPos, this);
 	}
 }
